e147103885912ae0902d14e1de8321843f06bd60
BayanDaboubash <bayan.daboubash2000@gmail.com>
2021-04-29 16:57:42 +0300

    home

---
+++ homejs.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 60,
+    stretch: 0,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
