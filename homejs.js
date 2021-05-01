bda9bbdd2c525f5712b0a493333d3fa7d66b60a9
BayanDaboubash <bayan.daboubash2000@gmail.com>
2021-05-01 17:14:09 +0300

    home

--- homejs.js
+++ homejs.js
@@ -8,88 +8,77 @@ let swiper = new Swiper(".swiper-container", {
   },
 });
 
-$("#All").on("click", () => {
-  let value = "All";
-  $(".item").show("1000");
-  $("#All").css({
-    "background-color": "orange",
+  $("#All").on("click", () => {
+    let value = "All";
+    $(".item").show("1000");
+    $("#All").css({
+      "background-color": "orange",
+    })
+    $("#Cat").css({
+      "background-color": "rgb(238, 238, 238)",
+    })
+    $("#Flower").css({
+      "background-color": "rgb(238, 238, 238)",
+    })
+    $("#Bird").css({
+      "background-color": "rgb(238, 238, 238)",
+    })
   });
-  $("#Cat").css({
-    "background-color": "rgb(238, 238, 238)",
-  });
-  $("#Flower").css({
-    "background-color": "rgb(238, 238, 238)",
-  });
-  $("#Bird").css({
-    "background-color": "rgb(238, 238, 238)",
-  });
-});
 
-$("#Cat").on("click", () => {
-  let value = "Cat";
-  $(".item")
-    .filter("." + value)
-    .show("1000");
-  $(".item")
-    .not("." + value)
-    .hide("1000");
-  $("#All").css({
-    "background-color": "rgb(238, 238, 238)",
+  $("#Cat").on("click", () => {
+    let value = "Cat";
+    $(".item").filter("."+value).show('1000');
+    $(".item").not("."+value).hide('1000');
+    $("#All").css({
+      "background-color": "rgb(238, 238, 238)",
+    })
+    $("#Cat").css({
+      "background-color": "orange",
+    })
+    $("#Flower").css({
+      "background-color": "rgb(238, 238, 238)",
+    })
+    $("#Bird").css({
+      "background-color": "rgb(238, 238, 238)",
+    })
   });
-  $("#Cat").css({
-    "background-color": "orange",
-  });
-  $("#Flower").css({
-    "background-color": "rgb(238, 238, 238)",
-  });
-  $("#Bird").css({
-    "background-color": "rgb(238, 238, 238)",
-  });
-});
 
-$("#Flower").on("click", () => {
-  let value = "Flower";
-  $(".item")
-    .filter("." + value)
-    .show("1000");
-  $(".item")
-    .not("." + value)
-    .hide("1000");
-  $("#All").css({
-    "background-color": "rgb(238, 238, 238)",
+  $("#Flower").on("click", () => {
+    let value = "Flower";
+    $(".item").filter("."+value).show('1000');
+    $(".item").not("."+value).hide('1000');
+    $("#All").css({
+      "background-color": "rgb(238, 238, 238)",
+    })
+    $("#Cat").css({
+      "background-color": "rgb(238, 238, 238)",
+    })
+    $("#Flower").css({
+      "background-color": "orange",
+    })
+    $("#Bird").css({
+      "background-color": "rgb(238, 238, 238)",
+    })
   });
-  $("#Cat").css({
-    "background-color": "rgb(238, 238, 238)",
-  });
-  $("#Flower").css({
-    "background-color": "orange",
-  });
-  $("#Bird").css({
-    "background-color": "rgb(238, 238, 238)",
-  });
-});
 
-$("#Bird").on("click", () => {
-  let value = "Bird";
-  $(".item")
-    .filter("." + value)
-    .show("1000");
-  $(".item")
-    .not("." + value)
-    .hide("1000");
-  $("#All").css({
-    "background-color": "rgb(238, 238, 238)",
+  $("#Bird").on("click", () => {
+    let value = "Bird";
+    $(".item").filter("."+value).show('1000');
+    $(".item").not("."+value).hide('1000');
+    $("#All").css({
+      "background-color": "rgb(238, 238, 238)",
+    })
+    $("#Cat").css({
+      "background-color": "rgb(238, 238, 238)",
+    })
+    $("#Flower").css({
+      "background-color": "rgb(238, 238, 238)",
+    })
+    $("#Bird").css({
+      "background-color": "orange",
+    })
   });
-  $("#Cat").css({
-    "background-color": "rgb(238, 238, 238)",
-  });
-  $("#Flower").css({
-    "background-color": "rgb(238, 238, 238)",
-  });
-  $("#Bird").css({
-    "background-color": "orange",
-  });
-});
+
 
 // $(document).ready(()=>{
 //   $("#All").on("click",()=>{
@@ -138,8 +127,10 @@ $("#edit").on("click", () => {
   $(".editPhoto").show();
 });
 
-const array = [];
+// const array = [];
+// let a=1;
+// $(".favourites").html("<img src='1.jpg'");
 
-$("img").on("dblclick", () => {
-  array.push($(this.id));
-});
+// $("img").on("dblclick", () => {
+//   $(".favourites").append($())
+// });
