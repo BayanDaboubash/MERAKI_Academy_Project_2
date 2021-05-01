972189541ee0371e271646ae048759589c58168e
BayanDaboubash <bayan.daboubash2000@gmail.com>
2021-05-01 16:57:24 +0300

    album

--- homejs.js
+++ homejs.js
@@ -8,24 +8,97 @@ let swiper = new Swiper(".swiper-container", {
   },
 });
 
-$(document).ready(()=>{
-  $(".list li").on("click",()=>{
-    let value = $(this).text();
-    console.log(value);
-    if (value == "All"){
-      $(".item").show('1000');
-    }
-    else{
-      $(".item").filter(value).show('1000');
-      $(".item").not(value).hide('1000');
-    }
-  })
 
-  $(".list").on("click", ()=>{
-    $(this).addClass('active').siblings().removeClass('active');
-  })
-})
+  $("#All").on("click", () => {
+    let value = "All";
+    $(".item").show("1000");
+    $("#All").css({
+      "background-color": "orange",
+    })
+    $("#Cat").css({
+      "background-color": "rgb(238, 238, 238)",
+    })
+    $("#Flower").css({
+      "background-color": "rgb(238, 238, 238)",
+    })
+    $("#Bird").css({
+      "background-color": "rgb(238, 238, 238)",
+    })
+  });
 
+  $("#Cat").on("click", () => {
+    let value = "Cat";
+    $(".item").filter("."+value).show('1000');
+    $(".item").not("."+value).hide('1000');
+    $("#All").css({
+      "background-color": "rgb(238, 238, 238)",
+    })
+    $("#Cat").css({
+      "background-color": "orange",
+    })
+    $("#Flower").css({
+      "background-color": "rgb(238, 238, 238)",
+    })
+    $("#Bird").css({
+      "background-color": "rgb(238, 238, 238)",
+    })
+  });
+
+  $("#Flower").on("click", () => {
+    let value = "Flower";
+    $(".item").filter("."+value).show('1000');
+    $(".item").not("."+value).hide('1000');
+    $("#All").css({
+      "background-color": "rgb(238, 238, 238)",
+    })
+    $("#Cat").css({
+      "background-color": "rgb(238, 238, 238)",
+    })
+    $("#Flower").css({
+      "background-color": "orange",
+    })
+    $("#Bird").css({
+      "background-color": "rgb(238, 238, 238)",
+    })
+  });
+
+  $("#Bird").on("click", () => {
+    let value = "Bird";
+    $(".item").filter("."+value).show('1000');
+    $(".item").not("."+value).hide('1000');
+    $("#All").css({
+      "background-color": "rgb(238, 238, 238)",
+    })
+    $("#Cat").css({
+      "background-color": "rgb(238, 238, 238)",
+    })
+    $("#Flower").css({
+      "background-color": "rgb(238, 238, 238)",
+    })
+    $("#Bird").css({
+      "background-color": "orange",
+    })
+  });
+
+
+// $(document).ready(()=>{
+//   $("#All").on("click",()=>{
+//     let value ="All";
+//     //let value = $(this).text();
+//     console.log(value);
+//     if (value == "All"){
+//       $(".item").show('1000');
+//     }
+//     else{
+//       $(".item").filter(value).show('1000');
+//       $(".item").not(value).hide('1000');
+//     }
+//   })
+
+//   $(".list").on("click", ()=>{
+//     $(this).addClass('active').siblings().removeClass('active');
+//   })
+// })
 
 $("#all").on("click", () => {
   $(".home").show();
@@ -57,7 +130,6 @@ $("#edit").on("click", () => {
 
 const array = [];
 
-$("img").on("dblclick",()=>{
-  array.push($(this.id))
-})
-
+$("img").on("dblclick", () => {
+  array.push($(this.id));
+});
