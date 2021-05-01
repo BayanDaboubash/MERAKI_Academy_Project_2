adc55e3f0115c81178de03ab329703a3da03e29b
BayanDaboubash <bayan.daboubash2000@gmail.com>
2021-05-01 15:29:51 +0300

    album1

--- homejs.js
+++ homejs.js
@@ -10,13 +10,13 @@ let swiper = new Swiper(".swiper-container", {
 
 $(document).ready(()=>{
   $(".list").on("click",()=>{
-    const value = $(this).attr('data-filter');
+    let value = $(this).attr('data-filter');
     if (value == "All"){
       $(".item").show('1000');
     }
     else{
-      $(".item").filter(value).show('1000');
-      $(".item").not(value).hide('1000');
+      $(".item").filter("."+value).show('1000');
+      $(".item").not("."+value).hide('1000');
     }
   })
 
