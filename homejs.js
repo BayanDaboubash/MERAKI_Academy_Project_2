e6e338085620b69ba0792ae06ed5379744c01025
BayanDaboubash <bayan.daboubash2000@gmail.com>
2021-05-01 13:53:35 +0300

    home1

--- homejs.js
+++ homejs.js
@@ -1,17 +1,10 @@
 let swiper = new Swiper(".swiper-container", {
   effect: "coverflow",
-  grabCursor: true,
   centeredSlides: true,
   loop: true,
   slidesPerView: "auto",
   coverflowEffect: {
     rotate: 60,
-    stretch: 0,
-    modifier: 1,
-    slideShadows: true,
-  },
-  pagination: {
-    el: ".swiper-pagination",
   },
 });
 
